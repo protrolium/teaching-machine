52da3f59418211d4fb28f63a8e08876609abbf89
TRAPPIST <in@gav.cloud>
2024-09-07 23:23:38 -0700

    modified zones foreach children method

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,5 +1,5 @@
 <?= $rockfrontend->renderIf("sections/default-page.latte", "template=default-page") ?>
 <?= $rockfrontend->renderIf("sections/tag.latte", "template=tag") ?>
 <?= $rockfrontend->renderIf("sections/zones.latte", "name=zones") ?>
-<?= $rockfrontend->renderIf("sections/zone.latte", "name=cinema|events|print|music|web|installation|virtual-reality|mixtape") ?>
+<?= $rockfrontend->renderIf("sections/zone.latte", "name=cinema|events|print|music|web|installation|virtual-reality|mixtape|cinema-of-the-void") ?>
 <?= $rockfrontend->renderIf("sections/about.latte", "name=about") ?>
